1bebac1b64e6cdf2dd024a2ee311be23300e79ed
Daniel Grieneisen <daniel@enigma.aero>
2025-11-10 14:46:12 -0500

    fix: fixed missing plugins for vehicle config

--- custom/cmake/CustomOverrides.cmake
+++ custom/cmake/CustomOverrides.cmake
@@ -1,4 +1,8 @@
-set(QGC_APP_NAME "Custom-QGroundControl" CACHE STRING "App Name" FORCE)
+set(QGC_APP_NAME "Nexus-QGC" CACHE STRING "App Name" FORCE)
+set(QGC_APP_COPYRIGHT "Copyright (c) 2025 Enigma Aerospace. All rights reserved." CACHE STRING "Copyright" FORCE)
+set(QGC_APP_DESCRIPTION "Ground Control App" CACHE STRING "Description" FORCE)
+set(QGC_ORG_NAME "Enigma Aerospace" CACHE STRING "Org Name" FORCE)
+set(QGC_ORG_DOMAIN "enigma.aero" CACHE STRING "Domain" FORCE)
 
 set(QGC_MACOS_ICON_PATH "${CMAKE_SOURCE_DIR}/custom/res" CACHE PATH "MacOS Icon Path" FORCE)
 set(QGC_APPIMAGE_ICON_PATH "${CMAKE_SOURCE_DIR}/custom/res/icons/custom_qgroundcontrol.png" CACHE FILEPATH "AppImage Icon Path" FORCE)
